4dd2c1a120e00a34011bfec729107aa3b71cbe2e
Anes Smajić <anes.smajic95@hotmail.com>
2021-10-24 20:17:43 +0200

    Init

---
+++ HtmlHelperExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Umbraco.Localizer
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string DictionaryItem<TModel>(this HtmlHelper<TModel> htmlHelper, string key)
+        {
+            if (HttpContext.Current.Request["keys"] == "true") return key;
+
+            var helper = Web.Composing.Current.UmbracoHelper;
+            var value = helper.GetDictionaryValue(key);
+            if (string.IsNullOrEmpty(value)) return key;
+            return value;
+        }
+
+        public static string DictionaryItemCulture<TModel>(this HtmlHelper<TModel> htmlHelper, string key, string culture)
+        {
+            if (HttpContext.Current.Request["keys"] == "true") return key;
+
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+            var helper = Web.Composing.Current.UmbracoHelper;
+            var value = helper.GetDictionaryValue(key);
+            if (string.IsNullOrEmpty(value)) return key;
+            return value;
+        }
+    }
+}
---
+++ Localizer.cs
@@ -0,0 +1,22 @@
+﻿using Umbraco.Web;
+
+namespace Umbraco.Localizer
+{
+    public class Localizer
+    {
+        private UmbracoHelper Helper { get; set; }
+
+        public Localizer()
+        {
+            Helper = Web.Composing.Current.UmbracoHelper;
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                return Helper.GetDictionaryValue(name);
+            }
+        }
+    }
+}
